093e6e5328f70dc2f695bd7b3f179e2ef425ce7d
joshua <jhecker2001@gmail.com>
2024-04-28 11:50:43 -0500

    Created files, added members to Account class.

---
+++ src/main/java/joshuahecker/bankapp/Account.java
@@ -0,0 +1,10 @@
+package joshuahecker.bankapp;
+import java.util.Scanner;
+
+public class Account {
+    int balance;
+    int transactionAmount;
+    String transactionType;
+    String customerName;
+    String customerId;
+}
---
+++ src/main/java/joshuahecker/bankapp/BankApp.java
@@ -0,0 +1,13 @@
+package joshuahecker.bankapp;
+import java.util.Scanner;
+
+public class BankApp {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Please enter your first name.");
+        
+        String name = input.nextLine();
+    }
+}
